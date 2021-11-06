f21a9bd911c7ed4ae811225800b9fd39325bd8ab
pedroluiznogueira <peluiznogueiram@hotmail.com>
2021-11-05 23:56:12 -0300

    Me comunicando para a API principal da Udeyou por este Microserviço por meio do Fe ign do Spring Cloud

---
+++ src/app/components/confirmar-cadastro/confirmar-cadastro.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { ConfirmarCadastroService } from 'src/app/services/confirmar-cadastro.service';
+
+@Component({
+  selector: 'app-confirmar-cadastro',
+  templateUrl: './confirmar-cadastro.component.html',
+  styleUrls: ['./confirmar-cadastro.component.css']
+})
+export class ConfirmarCadastroComponent implements OnInit {
+
+  constructor(
+    private confirmarCadastro: ConfirmarCadastroService
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  public emitirConfirmacao() {
+    this.confirmarCadastro.confirmarCadastro();
+  }
+
+}
